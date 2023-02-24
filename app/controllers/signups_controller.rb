817d1d8b9da1875783d329d2d516ac1b635fba70
Kyle Brackman <kyle.a.brackman@gmail.com>
2023-02-24 14:15:45 -0700

    commit tree 27d178c861fecbb23c4b1899f76dc6ae72b62d71

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,13 @@
+class SignupsController < ApplicationController
+
+    def create 
+        signup = Signup.create(signup_params)
+        render json: signup, status: :created
+    end
+
+    private
+
+    def signup_params
+        params.permit(:camper_id, :activity_id, :time)
+    end
+end
